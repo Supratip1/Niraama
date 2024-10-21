bcc1d92f8084044de347e0e2e295ca6dd6ef6361
unknown <supratipbhattacharya2@gmail.com>
2024-10-21 18:53:25 +0530

    frontend commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // This tells Tailwind to scan all files in your src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
